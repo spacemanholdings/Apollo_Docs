26ec5ca6362cf55c9fe7c527030e6beb3f4af51c
Alexis Falquier <endrefalquier@gmail.com>
2019-09-23 15:21:53 -0700

    fixed a bug

--- conf.py
+++ conf.py
@@ -13,11 +13,11 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-
+import sphinx_rtd_theme
 
 # -- Project information -----------------------------------------------------
 
-project = 'Apollo Docs'
+project = 'Spaceman ID Apollo'
 copyright = '2019, Spaceman ID'
 author = 'Spaceman ID'
 
@@ -30,7 +30,7 @@ release = '1.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx_rtd_theme",'recommonmark']
+extensions = ['recommonmark', 'sphinx_rtd_theme']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
---
+++ index.rst
@@ -0,0 +1,22 @@
+.. Spaceman ID Apollo documentation master file, created by
+   sphinx-quickstart on Mon Sep 23 15:10:16 2019.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Spaceman ID Apollo's documentation!
+==============================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   api-documentation
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
